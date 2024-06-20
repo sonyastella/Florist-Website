3841b002f07daa153b3410f3778a1b2893936c1e
sonyastella <161102066+sonyastella@users.noreply.github.com>
2024-06-20 12:26:42 -0700

    Update OurTeam.tsx

--- src/components/OurTeam/OurTeam.tsx
+++ src/components/OurTeam/OurTeam.tsx
@@ -29,11 +29,11 @@ const OurTeam: React.FC = () => {
 
   return (
     <div className="ourTeam bg-[#efe6dd] text-center py-20">
-      <h1 className="text-4xl font-bold text-[#5f5e7a] mb-20 font-alice">Meet our team</h1>
+      <h1 className="text-4xl font-bold text-[#5f5e7a] mb-20">Meet our team</h1>
       <div className="team-container flex flex-wrap justify-center gap-8">
         {teamMembers.map((member, index) => (
-          <div key={index} className="team-member w-full md:w-1/3 lg:w-1/4 text-center p-4 bg-white rounded-lg shadow-lg">
-            <img src={member.picture.large} alt={`${member.name.first} ${member.name.last}`} className="w-full h-64 object-cover rounded-lg mb-4" />
+          <div key={index} className="team-member w-32 h-32 text-center p-0 bg-[#efe6dd] rounded-lg shadow-lg">
+            <img src={member.picture.large} alt={`${member.name.first} ${member.name.last}`} className="w-32 h-32 object-cover rounded-lg mb-4" />
             <h2 className="text-xl font-semibold text-[#5f5e7a]">{`${member.name.first} ${member.name.last}`}</h2>
           </div>
         ))}
