6289a54c947264ec41198f107a236ed1c948c512
sonyastella <161102066+sonyastella@users.noreply.github.com>
2024-06-20 12:26:17 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,19 +15,17 @@ const App: React.FC = () => {
   return (
     <Provider store={store}>
       <Router>
-        <div className="flex flex-col min-h-screen">
+        <div className="app-container flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow">
-            <div className="max-w-screen-md mx-auto">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/catalogue" element={<Catalogue />} />
-                <Route path="/contact" element={<ContactUs />} />
-                <Route path="/team" element={<OurTeam />} />
-                <Route path="/about" element={<AboutUs />} />
-              </Routes>
-            </div>
-          </main>
+          <div className="flex-grow max-w-screen-md mx-auto">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/catalogue" element={<Catalogue />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route path="/team" element={<OurTeam />} />
+              <Route path="/about" element={<AboutUs />} />
+            </Routes>
+          </div>
           <Footer />
         </div>
       </Router>
