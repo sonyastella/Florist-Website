d9989b41707288fff77282dd1fbee8ec9a4a34b4
sonyastella <161102066+sonyastella@users.noreply.github.com>
2024-06-19 23:08:00 -0700

    Update catalogueSlice.ts

--- src/features/catalogue/catalogueSlice.ts
+++ src/features/catalogue/catalogueSlice.ts
@@ -14,8 +14,15 @@ interface CatalogueState {
 }
 
 const initialState: CatalogueState = {
-  items: [],
-  status: 'idle',
+  items: [
+    { "id": 1, "name": "Royal Velvet Bouquet", "image": "/images/flower2.png", "price": "Rp. 2,800,000" },
+    { "id": 2, "name": "Romantic Blushing Petals", "image": "/images/flower3.png", "price": "Rp. 1,000,000" },
+    { "id": 3, "name": "Beautiful Rainbow Baby Breathe", "image": "/images/flower4.png", "price": "Rp. 1,500,000" },
+    { "id": 4, "name": "Pink Blush Baby Breathe Bouquet", "image": "/images/flower5.png", "price": "Rp. 1,200,000" },
+    { "id": 5, "name": "Azure Elegance Bouquet", "image": "/images/flower6.png", "price": "Rp. 1,700,000" },
+    { "id": 6, "name": "Ocean Blue Baby Breathe Bouquet", "image": "/images/flower7.png", "price": "Rp. 1,450,000" }
+  ],
+  status: 'succeeded',
   error: null,
 };
 
@@ -36,10 +43,5 @@ const catalogueSlice = createSlice({
 });
 
 export const { setCatalogue, setStatus, setError } = catalogueSlice.actions;
+
 export default catalogueSlice.reducer;
-
-
-
-
-
-//Start the json-server: "json-server --watch db.json --port 5000"
