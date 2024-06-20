ba062620e90450028b86e56739fa30220bb50832
sonyastella <161102066+sonyastella@users.noreply.github.com>
2024-06-19 23:08:31 -0700

    Update Catalogue.tsx

--- src/components/Catalogue/Catalogue.tsx
+++ src/components/Catalogue/Catalogue.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
-
-// Define your catalogue data
-const catalogueData = [
-  { "id": 1, "name": "Royal Velvet Bouquet", "image": "/images/flower2.png", "price": "Rp. 2,800,000" },
-  { "id": 2, "name": "Romantic Blushing Petals", "image": "/images/flower3.png", "price": "Rp. 1,000,000" },
-  { "id": 3, "name": "Beautiful Rainbow Baby Breathe", "image": "/images/flower4.png", "price": "Rp. 1,500,000" },
-  { "id": 4, "name": "Pink Blush Baby Breathe Bouquet", "image": "/images/flower5.png", "price": "Rp. 1,200,000" },
-  { "id": 5, "name": "Azure Elegance Bouquet", "image": "/images/flower6.png", "price": "Rp. 1,700,000" },
-  { "id": 6, "name": "Ocean Blue Baby Breathe Bouquet", "image": "/images/flower7.png", "price": "Rp. 1,450,000" }
-];
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import './Catalogue.css';
 
 const Catalogue: React.FC = () => {
+  const { items, status, error } = useSelector((state: RootState) => state.catalogue);
+
   return (
     <div className="py-20 bg-[#efe6dd] text-center font-alice">
       <h1 className="text-4xl font-bold text-[#5f5e7a] mb-20">Catalogue Page</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16 max-w-6xl mx-auto px-4">
-        {catalogueData.map((item) => (
-          <div key={item.id} className="text-center p-4 bg-white shadow-lg rounded-lg">
-            <img src={item.image} alt={item.name} className="w-full h-auto object-cover rounded-lg mb-4" />
-            <h2 className="text-xl font-semibold text-[#333]">{item.name}</h2>
-            <p className="text-lg text-gray-600">{item.price}</p>
-          </div>
-        ))}
-      </div>
+      {status === 'loading' && <p>Loading...</p>}
+      {status === 'failed' && <p>{error}</p>}
+      {status === 'succeeded' && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto px-4">
+          {items.map((item) => (
+            <div key={item.id} className="text-center p-4 bg-white shadow-lg rounded-lg">
+              <img src={item.image} alt={item.name} className="w-full h-64 object-cover rounded-lg mb-4" />
+              <h2 className="text-xl font-semibold text-[#333]">{item.name}</h2>
+              <p className="text-lg text-gray-600">{item.price}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
