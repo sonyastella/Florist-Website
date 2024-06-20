05456d0e5e7585900957b61e40b51d037794bdee
sonyastella <161102066+sonyastella@users.noreply.github.com>
2024-06-20 07:57:38 -0700

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,15 +9,15 @@ const Home: React.FC = () => {
       </p>
       <div className="home-content flex flex-wrap justify-center gap-8">
         <div className="flower-block w-full md:w-1/3 text-center p-4">
-          <img src="/images/flower4.png" alt="Flower 1" width="200" height="300" className="w-full h-auto object-cover rounded-lg mb-4" />
+          <img src="/images/flower4.webp" alt="Flower 1" width="200" height="300" className="w-full h-auto object-cover rounded-lg mb-4" />
           <h2 className="text-xl font-semibold text-[#5f5e7a]">Azure Elegance Bouquet</h2>
         </div>
         <div className="flower-block w-full md:w-1/3 text-center p-4">
-          <img src="/images/flower2.png" alt="Flower 2" width="200" height="300" className="w-full h-auto object-cover rounded-lg mb-4" />
+          <img src="/images/flower2.webp" alt="Flower 2" width="200" height="300" className="w-full h-auto object-cover rounded-lg mb-4" />
           <h2 className="text-xl font-semibold text-[#5f5e7a]">Romantic Blushing Petals</h2>
         </div>
         <div className="flower-block w-full md:w-1/3 text-center p-4">
-          <img src="/images/flower3.png" alt="Flower 3" width="200" height="300" className="w-full h-auto object-cover rounded-lg mb-4" />
+          <img src="/images/flower3.webp" alt="Flower 3" width="200" height="300" className="w-full h-auto object-cover rounded-lg mb-4" />
           <h2 className="text-xl font-semibold text-[#5f5e7a]">Royal Velvet Bouquet</h2>
         </div>
       </div>
